9ce526bb917bc233bb4c6b2c49d693504f71b272
seanluse <sean@kintone.com>
2026-01-15 18:56:33 +0900

    first

---
+++ eslint.config.mjs
@@ -0,0 +1,4 @@
+import kintoneCustomizePrettier from "@cybozu/eslint-config/flat/presets/kintone-customize-prettier.js";
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [...kintoneCustomizePrettier];
