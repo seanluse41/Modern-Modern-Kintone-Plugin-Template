7d18ff07813b6dba8dbd2ebe3a25e46beb4563e1
seanluse41 <seanluse41@gmail.com>
2026-01-18 00:33:59 +0900

    i18n and kintone ui components

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -1,6 +1,9 @@
+import { setupI18n } from '../i18n.js';
+
 const PLUGIN_ID = kintone.$PLUGIN_ID;
 
-kintone.events.on('app.record.index.show', () => {
+kintone.events.on('app.record.index.show', async () => {
+  const i18n = await setupI18n();
   const config = kintone.plugin.app.getConfig(PLUGIN_ID);
 
   const spaceElement = kintone.app.getHeaderSpaceElement();
@@ -15,7 +18,7 @@ kintone.events.on('app.record.index.show', () => {
   messageEl.classList.add('plugin-space-message');
   messageEl.textContent = config.message;
   headingEl.classList.add('plugin-space-heading');
-  headingEl.textContent = 'Hello kintone plugin!';
+  headingEl.textContent = i18n.t('helloKintone');
 
   fragment.appendChild(headingEl);
   fragment.appendChild(messageEl);
